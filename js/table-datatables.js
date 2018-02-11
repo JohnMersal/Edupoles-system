f3731f3c36858ec87e6961b2c2e0f80e99a88271
John G Mersal <john@eseed.net>
2018-02-11 15:01:28 +0200

    new

--- js/table-datatables.js
+++ js/table-datatables.js
@@ -2,7 +2,7 @@
 $(function () {
 	// Init
     var spinner = $( ".spinner" ).spinner();
-    var table = $('#table_id').dataTable( {
+    var table = $('#table_id1, #table_id ').dataTable( {
         "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
     } );
 
